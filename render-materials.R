16655efef01ede5dcb0d5de216da77ff8923fb40
Lars Schöbitz <lschoebitz@ethz.ch>
2023-09-06 15:59:31 +0200

    Update site

--- render-materials.R
+++ render-materials.R
@@ -18,6 +18,8 @@ walk(rmds, render)
 xaringans <- dir_info(recurse = 3, glob = "slides/*.Rmd") %>% 
   filter(str_detect(path, "slides")) %>%
   filter(!str_detect(path, "setup")) %>%
+  filter(!str_detect(path, "e1_d06-2")) |> 
+  filter(!str_detect(path, "e1_d99")) |> 
   pull(path)
 
 walk(xaringans, render)
